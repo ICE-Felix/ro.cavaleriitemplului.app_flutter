48faeff9effd211b9f3c3270ba3a46b9bc8c6916
Theodor Stoica <stoicath@gmail.com>
2025-10-15 12:41:29 +0300

    Solved import for profile dialogs

--- lib/features/profile/presentation/widgets/profile_account_section.dart
+++ lib/features/profile/presentation/widgets/profile_account_section.dart
@@ -1,7 +1,7 @@
+import 'package:app/features/profile/presentation/widgets/logout_confirmation_dialog/logout_confirmation_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:app/features/profile/presentation/widgets/profile_menu_item.dart';
-import 'package:app/features/profile/presentation/widgets/logout_confirmation_dialog.dart';
 
 class ProfileAccountSection extends StatelessWidget {
   const ProfileAccountSection({super.key});
--- lib/features/profile/presentation/widgets/profile_settings_section.dart
+++ lib/features/profile/presentation/widgets/profile_settings_section.dart
@@ -1,7 +1,7 @@
+import 'package:app/features/profile/presentation/widgets/language_selection_dialog/language_selection_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:app/features/profile/presentation/widgets/profile_menu_item.dart';
-import 'package:app/features/profile/presentation/widgets/language_selection_dialog.dart';
 
 class ProfileSettingsSection extends StatelessWidget {
   const ProfileSettingsSection({super.key});
